c3be81b302f139242f78920593de7960f73cf913
dexter122690 <dexterbsanagustin@gmail.com>
2026-08-01 23:13:36 +0800

    Configure Supabase public client

---
+++ app-config.js
@@ -0,0 +1,8 @@
+/*
+  Public browser configuration.
+  This uses the Supabase publishable key only. Never add a service_role key.
+*/
+window.BUSINESS_WEB_CENTER_SUPABASE = {
+  url: 'https://bcgrzfrxuccvmznafake.supabase.co',
+  publishableKey: 'sb_publishable_jNlv1zVj4lBbbBBgVMYTug_zBLiCfOK'
+};
